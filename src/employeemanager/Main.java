9fc4999f412b8e8ede73db9e14962c6ee32ccbbd
Arianit Bobaj <arian@Arianit>
2017-06-17 18:35:39 +0200

    Main Class

--- src/employeemanager/Main.java
+++ src/employeemanager/Main.java
@@ -2,11 +2,19 @@ package employeemanager;
 
 public class Main {
     
-    
+    private static Admin admin = null;
     public static void main(String[] args) {
-        MainWindow jFameEmployeeList = new MainWindow();
-        jFameEmployeeList.setVisible(true);
-        jFameEmployeeList.setDefaultCloseOperation(MainWindow.EXIT_ON_CLOSE);
+        LogInWindow loginWindow = new LogInWindow();
+        loginWindow.setVisible(true);
+        
+        loginWindow.setLoginCallback((Admin adminParam) -> {
+            
+            Main.admin = adminParam;
+            
+            MainWindow jFrameEmployeeList = new MainWindow();
+            jFrameEmployeeList.setVisible(true);
+            jFrameEmployeeList.setDefaultCloseOperation(MainWindow.EXIT_ON_CLOSE);
+        });
     }
     
 }
